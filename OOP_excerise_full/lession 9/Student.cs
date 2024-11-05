af089f564b6c6e0dddeeda503e5df3670d932006
huylebron <doquochuy27102003@gmail.com>
2024-11-05 15:21:53 +0700

    create class student

---
+++ OOP_excerise_full/lession 9/Student.cs
@@ -0,0 +1,20 @@
+﻿namespace OOP_excerise_full.lession_9 ;
+
+public class Student
+{
+    public string  Name { get ; set ; }
+    public int Age  { get ; set ; }
+    public double Math { get ; set ; }
+    public double Lit { get ; set ; }
+
+
+    public Student ( string name , int age , double math , double lit )
+    {
+        Name = name ;
+        Age = age ;
+        Math = math ;
+        Lit = lit ;
+        
+    }
+    
+}
